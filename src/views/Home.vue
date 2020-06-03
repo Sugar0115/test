a58006299a2db7c41a5b26fdb24cf22821f54437
Sugar0115 <779272164@qq.com>
2020-06-03 19:30:40 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tinymce :height='500'  ref='Tinymce'/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import tinymce from '@/components/Tinymce'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    tinymce
   }
 }
 </script>
